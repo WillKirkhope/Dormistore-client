1f4834c06027baa51695b76104e9d348ffaa6563
WillKirkhope <kirkhopetraining@yahoo.com>
2018-08-08 15:29:53 -0600

    Added create form component

---
+++ storefront/src/components/Create.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Create extends Component {
+  render(){
+    return (
+      <form className="create-form" onSubmit={props.}>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" />
+        <label htmlFor="description">Description</label>
+        <input type="text" name="description" />
+        <label htmlFor="image">Image</label>
+        <input type="text" name="imageUrl" />
+        <label htmlFor="price">Price</label>
+        <input type= integer name="price" />
+        <label htmlFor="quantity">Quantity/label>
+        <input type="integer" name="quantity" />
+        <input type="submit" name="submit" value="Submit" />
+
+      </form>
+    )
+  }
+}
+
+export default Create;
