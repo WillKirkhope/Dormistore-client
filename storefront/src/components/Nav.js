b8b979424dab44636cc11357c57f9ca3424b9944
WillKirkhope <kirkhopetraining@yahoo.com>
2018-08-08 12:01:17 -0600

    Added hamburger menu

--- storefront/src/components/Nav.js
+++ storefront/src/components/Nav.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from 'react'
+import { slide as Menu } from 'react-burger-menu'
 
-const Nav = (props) => {
-  return (
-    <div>
-      <nav className="nav-bar">
-        <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' />
-        <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' />
-        <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' />
-      </nav>
-    </div>
-  )
+class Nav extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+  }
+
+  render () {
+    return (
+      <Menu>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
+    );
+  }
 }
 
-export default Nav;
+export default Nav
