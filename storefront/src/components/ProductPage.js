5339cf520746977ac042f30a3581e727c6e65533
WillKirkhope <kirkhopetraining@yahoo.com>
2018-08-10 10:13:57 -0600

    Added ProductPage additions

--- storefront/src/components/ProductPage.js
+++ storefront/src/components/ProductPage.js
@@ -8,7 +8,14 @@ class ProductPage extends Component {
       <div className="Home">
       <Header />
       <main>
-      <h1>PP...</h1>
+        <div className="full-product">
+          <h1 className="full-product-name">Product Name</h1>
+          <img className="full-product-image" />
+          <h3 className="full-product-description"></h3>
+          <h4 className="price"></h4>
+          <h4 className="quantity"></h4>
+          <button>Add To Order</button>
+        </div>
       </main>
       <Footer />
       </div>
